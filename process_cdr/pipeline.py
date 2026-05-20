e0a11a8d8ee94fa52c8945561dcb2a127059ddd5
lionelgendebien <lionelgendebien@gmail.com>
2026-05-20 16:02:24 +0200

    added pipeline code again

--- process_cdr/pipeline.py
+++ process_cdr/pipeline.py
@@ -7,7 +7,7 @@ import config
 from openhexa.sdk.utils import Environment, get_environment
 
 
-@pipeline(name="process-cdr")
+@pipeline("process-cdr", name="process-cdr")
 @parameter(
     "cdr_raw_dir",
     name="Dossier des CDR",
